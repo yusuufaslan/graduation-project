8f8036b0b5a6ab1d6375e3065620e39cf758528d
yusuufaslan <yusuufaslan19@gmail.com>
2024-03-10 18:37:02 +0300

    sending files with form-data poc is done on welcome page.

---
+++ src/pages/Welcome/FileUploadComponent.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import axios from "axios"; // Import Axios for making HTTP requests
+
+function FileUploadComponent() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState("");
+
+  // Function to handle file selection
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  // Function to handle file upload
+  // Function to handle file upload
+  const handleUpload = async () => {
+    if (selectedFile) {
+      try {
+        setUploadStatus("Uploading...");
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+        formData.append("number", 2);
+
+        // Log FormData content
+        formData.forEach((value, key) => {
+          console.log(key, value);
+        });
+
+        const response = await axios.post(
+          "https://jsonplaceholder.typicode.com/posts",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+
+        console.log("Response from server:", response.data); // Log the response data
+        setUploadStatus("File uploaded successfully");
+      } catch (error) {
+        // Handle error
+        setUploadStatus("Error uploading file");
+        console.error("Error uploading file:", error);
+      }
+    } else {
+      // Handle case where no file is selected
+      setUploadStatus("No file selected");
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      <p>{uploadStatus}</p>
+    </div>
+  );
+}
+
+export default FileUploadComponent;
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Footer from '../../components/footer/Footer'
 import Navbar from '../../components/header/Navbar'
+import FileUploadComponent from './FileUploadComponent'
 
 function Welcome() {
   return (
     <div>
         <Navbar />
+        <FileUploadComponent />
         <Footer />
     </div>
   )
