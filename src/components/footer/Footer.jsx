b4609defe897659517c4eebdf2f65b3ac14004d1
yusuufaslan <yusuufaslan19@gmail.com>
2024-03-12 23:57:20 +0300

    footer is aligned as navbar w-7xl

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className='absolute bottom-0 w-full text-white px-3 sm:px-6 md:px-7 lg:px-8 py-4 bg-gray-800'>
-      <div className="container mx-auto"> {/* Added container to center content */}
+      <div className="container mx-auto max-w-7xl"> {/* Added container to center content */}
         © 2024, Yusuf Aslan
       </div>
     </div>
