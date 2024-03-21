e65d6e5ccd8edfbadb7130430936b10853802255
yusuufaslan <yusuufaslan19@gmail.com>
2024-03-22 00:44:51 +0300

    signin button in welcomenavbar is fixed.

--- src/components/header/WelcomeNavbar.jsx
+++ src/components/header/WelcomeNavbar.jsx
@@ -1,12 +1,13 @@
-// WelcomeNavbar.jsx
+// WelcomeNavbar.js
+
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import ViteLogo from "../../../public/vite.svg";
 
 export default function WelcomeNavbar() {
   return (
-    <div className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-4">
+    <div className="bg-gray-800 relative z-10"> {/* Add relative and z-10 classes */}
+      <div className="mx-auto max-w-7xl px-14">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-shrink-0 items-center">
             <img className="h-8 w-auto" src={ViteLogo} alt="Your Company" />
@@ -14,7 +15,7 @@ export default function WelcomeNavbar() {
           <div>
             <Link
               to="/sign-in"
-              className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out"
+              className="inline-block bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out"
             >
               Sign In
             </Link>
