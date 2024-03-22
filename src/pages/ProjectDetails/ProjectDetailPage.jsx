b23cc5d7bfd5b83deea92f83555b41e5effe56e8
yusuufaslan <yusuufaslan19@gmail.com>
2024-03-22 15:53:42 +0300

    proposal creation button is addded to the project detail page. if the user has not permission to access datasets

--- src/pages/ProjectDetails/ProjectDetailPage.jsx
+++ src/pages/ProjectDetails/ProjectDetailPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Navbar from "../../components/header/Navbar";
-import Footer from "../../components/footer/Footer";
 
 const ProjectDetailPage = () => {
   const { projectId } = useParams();
+  const navigate = useNavigate();
 
   const [project, setProject] = useState(null);
+  const [hasAccess, setHasAccess] = useState(false); // State to track access permission
 
   useEffect(() => {
     // Fetch project data from backend using projectId
@@ -40,7 +41,18 @@ const ProjectDetailPage = () => {
     };
 
     setProject(dummyProject);
+
+    // Check if user's email is in the project's emails array
+    const userEmail = "user@example.com"; // Replace this with actual user email
+    if (dummyProject.emails.includes(userEmail)) {
+      setHasAccess(true);
+    }
   }, [projectId]);
+
+  const handleCreateProposal = () => {
+    // Navigate to the proposal page with the projectId
+    navigate(`/proposal/${projectId}`);
+  };
 
   if (!project) {
     return <div>Loading...</div>;
@@ -48,78 +60,93 @@ const ProjectDetailPage = () => {
 
   return (
     <>
-    <Navbar />
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-semibold mb-6 text-center">
-        Project Detail - {project.id}
-      </h1>
-      <div className="max-w-7xl mx-auto bg-white shadow-md rounded-lg overflow-hidden border-2 mb-44">
-        <div className="px-6 py-4">
-          <p className="text-2xl font-bold mb-4">Project Information</p>
-          <div className="mb-4">
-            <p className="text-gray-700">
-              <span className="font-bold">Name:</span> <span>{project.name}</span>
-            </p>
-          </div>
-          <div className="mb-4">
-            <span className="text-gray-700 font-bold">Description:</span>{" "}
-            <p className="text-gray-700 font-normal">{project.description}</p>
-          </div>
-          <div className="mb-4">
-            <span className="text-gray-700 font-bold">Abstract:</span>{" "}
-            <p className="text-gray-700 font-normal">{project.abstract}</p>
-          </div>
-          <div className="mb-4">
-            <p className="text-gray-700">
-              <span className="font-bold">Owner:</span> <span className="font-normal">{project.owner}</span>
-            </p>
-          </div>
-          <div className="mb-4">
-            <p className="text-gray-700">
-              <span className="font-bold">Tags:</span>{" "}
-              <span className="flex flex-wrap mt-2">
-                {project.selectedTags.map((tagId) => (
-                  <span
-                    key={tagId}
-                    className="bg-blue-200 text-blue-800 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2"
-                  >
-                    Tag {tagId}
-                  </span>
-                ))}
-              </span>
-            </p>
-          </div>
-          <div className="mb-4">
-            <p className="text-gray-700">
-              <span className="font-bold">User Emails:</span>{" "}
-              <span className="flex flex-wrap mt-2">
-                {project.emails.map((email, index) => (
-                  <span
-                    key={index}
-                    className="bg-gray-200 text-gray-800 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2"
-                  >
-                    {email}
-                  </span>
-                ))}
-              </span>
-            </p>
-          </div>
-        </div>
-        <div className="px-6 py-4 border-t border-gray-200">
-          <p className="text-2xl font-bold mb-4">Datasets</p>
-          {project.datasets.map((dataset) => (
-            <div
-              key={dataset.id}
-              className="border-b border-gray-200 last:border-0 py-4"
-            >
-              <h3 className="text-xl font-semibold mb-2">{dataset.name}</h3>
-              <p className="text-gray-700">{dataset.description}</p>
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-semibold mb-6 text-center">
+          Project Detail - {project.id}
+        </h1>
+        <div className="max-w-7xl mx-auto bg-white shadow-md rounded-lg overflow-hidden border-2 mb-44">
+          <div className="px-6 py-4">
+            <p className="text-2xl font-bold mb-4">Project Information</p>
+            <div className="mb-4">
+              <p className="text-gray-700">
+                <span className="font-bold">Name:</span>{" "}
+                <span>{project.name}</span>
+              </p>
             </div>
-          ))}
+            <div className="mb-4">
+              <span className="text-gray-700 font-bold">Description:</span>{" "}
+              <p className="text-gray-700 font-normal">{project.description}</p>
+            </div>
+            <div className="mb-4">
+              <span className="text-gray-700 font-bold">Abstract:</span>{" "}
+              <p className="text-gray-700 font-normal">{project.abstract}</p>
+            </div>
+            <div className="mb-4">
+              <p className="text-gray-700">
+                <span className="font-bold">Owner:</span>{" "}
+                <span className="font-normal">{project.owner}</span>
+              </p>
+            </div>
+            <div className="mb-4">
+              <p className="text-gray-700">
+                <span className="font-bold">Tags:</span>{" "}
+                <span className="flex flex-wrap mt-2">
+                  {project.selectedTags.map((tagId) => (
+                    <span
+                      key={tagId}
+                      className="bg-blue-200 text-blue-800 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2"
+                    >
+                      Tag {tagId}
+                    </span>
+                  ))}
+                </span>
+              </p>
+            </div>
+            <div className="mb-4">
+              <p className="text-gray-700">
+                <span className="font-bold">User Emails:</span>{" "}
+                <span className="flex flex-wrap mt-2">
+                  {project.emails.map((email, index) => (
+                    <span
+                      key={index}
+                      className="bg-gray-200 text-gray-800 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2"
+                    >
+                      {email}
+                    </span>
+                  ))}
+                </span>
+              </p>
+            </div>
+            <div className="py-4 border-t border-gray-200">
+              {hasAccess ? (
+                <p className="text-green-600 font-bold">
+                  You have access to download datasets.
+                </p>
+              ) : (
+                <button
+                  onClick={handleCreateProposal}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Create Proposal to get access to download datasets.
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="px-6 py-4 border-t border-gray-200">
+            <p className="text-2xl font-bold mb-4">Datasets</p>
+            {project.datasets.map((dataset) => (
+              <div
+                key={dataset.id}
+                className="border-b border-gray-200 last:border-0 py-4"
+              >
+                <h3 className="text-xl font-semibold mb-2">{dataset.name}</h3>
+                <p className="text-gray-700">{dataset.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-    <Footer />
     </>
   );
 };
