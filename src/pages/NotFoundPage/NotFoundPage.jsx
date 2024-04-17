89b84b2adbb3982ba43853a8cefc6e5035790e41
yusuufaslan <yusuufaslan19@gmail.com>
2024-04-17 10:36:41 +0300

    404 not found page ui font sizes fixed.

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -5,16 +5,16 @@ const NotFoundPage = () => {
   return (
     <>
       {/*
-        This example requires updating your template:
+        
 
         ```
         <html class="h-full">
         <body class="h-full">
         ```
       */}
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <h1 className="text-lg font-semibold text-indigo-600">404</h1>
+          <h1 className="text-5xl font-semibold text-indigo-600">404</h1>
           <h2 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h2>
           <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -26,7 +26,7 @@ const NotFoundPage = () => {
             </a>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 };
