fddea5b6b03ac2198d8d071461f8dc7c80a8ec2d
Yusuf Aslan <yusuufaslan19@gmail.com>
2024-04-22 15:58:32 +0300

    add dataset button ui size and margins are fixed.

--- src/pages/EditProject/EditProjectForm.jsx
+++ src/pages/EditProject/EditProjectForm.jsx
@@ -211,9 +211,9 @@ const EditProjectForm = () => {
             )}
             <button
               onClick={handleAddDataset}
-              className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+              className="text-lg bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
             >
-              Add Dataset
+              + Add New Dataset
             </button>
           </div>
         </div>
