9ddd3c6efa08644c4d6b521e9822607c8a64ffaa
Yusuf Aslan <yusuufaslan19@gmail.com>
2024-04-28 20:20:43 +0300

    vite.config.js is modified, preview server conf is added.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    port: 5173,
+    strictPort: true,
+   },
+   server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+   },
 })
