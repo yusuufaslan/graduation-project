5bb3e90744c72be791d2f8a8c5f2fdbd2f843b05
Yusuf Aslan <yusuufaslan19@gmail.com>
2024-04-29 14:18:33 +0300

    set & clear filter button ui is made responsive for smaller devices.

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -151,19 +151,22 @@ const Explore = () => {
             />
             <br />
 
-            <button
-              onClick={fetchData}
-              className="bg-blue-500 text-white px-3 py-2 rounded-md text-sm"
-            >
-              Set Filters
-            </button>
+            <div className="flex flex-wrap gap-x-2 gap-y-2">
+				<button
+				onClick={fetchData}
+				className="bg-blue-500 text-white px-3 py-2 rounded-md text-sm"
+				>
+				Set Filters
+				</button>
 
-            <button
-              onClick={(e) => handleClearFilters(e)}
-              className="bg-red-500 text-white px-3 py-2 rounded-md text-sm ml-2"
-            >
-              Clear Filters
-            </button>
+				<button
+				onClick={(e) => handleClearFilters(e)}
+				className="bg-red-500 text-white px-3 py-2 rounded-md text-sm"
+				>
+				Clear Filters
+				</button>
+            </div>
+            
           </div>
           <div className="w-full md:w-3/4">
             <div className="mt-1">
