d1fb050bcfd61141a973cd2dcc1f25b6eae46789
Yusuf Aslan <yusuufaslan19@gmail.com>
2024-05-05 16:25:35 +0300

    message description info is added to confirmation modal page, also when user touches outside of the modal, it closes.

--- src/components/ConfirmationModal/ConfirmationModal.jsx
+++ src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const ConfirmationModal = ({ message, messageDescription = "", onConfirm, onCancel }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onCancel();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onCancel]);
+
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -8,9 +24,10 @@ const ConfirmationModal = ({ message, messageDescription = "", onConfirm, onCanc
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
 
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
 
         <div
+          ref={modalRef}
           className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
           role="dialog"
           aria-modal="true"
@@ -19,23 +36,17 @@ const ConfirmationModal = ({ message, messageDescription = "", onConfirm, onCanc
           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3
-                  className="text-lg leading-6 font-medium text-gray-900"
-                  id="modal-headline"
-                >
+                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
                   {message}
                 </h3>
               </div>
             </div>
           </div>
 
-          <div className="bg-white px-6">
+          <div className="bg-white px-6 mb-4">
             <div className="sm:flex sm:items-start">
               <div className="text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3
-                  className="text-md leading-6 font-normal text-gray-600"
-                  id="modal-headline"
-                >
+                <h3 className="text-md leading-6 font-normal text-gray-600" id="modal-headline">
                   {messageDescription}
                 </h3>
               </div>
