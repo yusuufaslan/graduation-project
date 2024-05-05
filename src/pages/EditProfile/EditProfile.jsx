287abe8b702dc1da688123a8b6cbc83b264cec30
Yusuf Aslan <yusuufaslan19@gmail.com>
2024-05-05 16:40:58 +0300

    user profile edit page is activated, and design is modified; but functionality is not ready yet.

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -100,7 +100,7 @@ function EditProfile() {
                 </label>
                 <input
                   type="text"
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
+                  className="mt-1 px-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
                   name="name"
                   value={formData.name}
                   onChange={handleChange}
@@ -112,7 +112,7 @@ function EditProfile() {
                 </label>
                 <input
                   type="text"
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
+                  className="mt-1  px-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
                   name="surname"
                   value={formData.surname}
                   onChange={handleChange}
@@ -124,7 +124,7 @@ function EditProfile() {
                 </label>
                 <input
                   type="email"
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
+                  className="mt-1  px-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
                   name="email"
                   value={formData.email}
                   onChange={handleChange}
@@ -136,7 +136,7 @@ function EditProfile() {
                   Role
                 </label>
                 <select
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
+                  className="mt-1  px-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
                   name="role"
                   value={formData.role}
                   onChange={handleChange}
@@ -153,7 +153,7 @@ function EditProfile() {
                   Institution
                 </label>
                 <select
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
+                  className="mt-1  px-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10"
                   name="institution"
                   value={formData.institution}
                   onChange={handleChange}
@@ -171,7 +171,7 @@ function EditProfile() {
                   Address
                 </label>
                 <textarea
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-20"
+                  className="mt-1 px-1  block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-20"
                   name="address"
                   value={formData.address}
                   onChange={handleChange}
@@ -180,7 +180,7 @@ function EditProfile() {
               <div className="flex justify-end">
                 <button
                   type="submit"
-                  className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-md text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Save
                 </button>
--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import { BsPersonCircle } from "react-icons/bs";
+import { AiOutlineEdit } from "react-icons/ai";
 import Navbar from "../../components/header/Navbar";
 
 function UserProfile() {
@@ -124,14 +125,15 @@ function UserProfile() {
                 <p className="mt-1 text-sm text-gray-900">{user.address}</p>
               </div>
               {/* Button to edit profile */}
-              {/* <div className="flex justify-end">
+              <div className="flex justify-end">
                 <button
                   onClick={handleEditProfile}
                   className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
+                  <AiOutlineEdit className="text-lg mr-2"/>
                   Edit Profile
                 </button>
-              </div> */}
+              </div>
             </div>
           </div>
         </div>
