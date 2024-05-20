7f7995beb792aef6e9c46f6de50f3bd2ad89ad4d
Yusuf Aslan <yusuufaslan19@gmail.com>
2024-05-20 18:45:01 +0300

    Sign in button fixed in Welcome page

--- src/pages/Welcome/HomeContent.jsx
+++ src/pages/Welcome/HomeContent.jsx
@@ -47,7 +47,7 @@ function HomeContent({ signedIn }) {
                 href="/sign-in"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Explore Projects & Datasets
+                Sign In
               </a>
             )}
             {signedIn && (
