2c7a0d45c2fa32c6f5e346d3fc461587a9793d89
Yusuf Aslan <yusuufaslan19@gmail.com>
2024-06-02 19:17:20 +0300

    no action selection bug is fixed.

--- src/pages/CreateDataset/CreateDataset.jsx
+++ src/pages/CreateDataset/CreateDataset.jsx
@@ -386,7 +386,7 @@ const CreateDataset = () => {
                     value={dataset.columnActions[columnIndex] || "none"}
                     onChange={(e) => handleColumnActionChange(e, columnIndex)}
                     className="border border-gray-400 rounded-md p-1 w-full font-normal text-sm"
-                    disabled={autoSelectedColumns.includes(columnName) && (dataset.columnActions[columnIndex] === "none" || dataset.columnActions[columnIndex] === undefined)}
+                    // disabled={autoSelectedColumns.includes(columnName) && (dataset.columnActions[columnIndex] === "none" || dataset.columnActions[columnIndex] === undefined)}
                   >
                     {/* <option value="none" disabled={autoSelectedColumns.includes(columnName)}>No Action</option> */}
                     <option value="none">No Action</option>
